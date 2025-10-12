e1c972a2a0322efefacc65891c5ec494364e2bb0
Bejoy Kachappilly <bejoy@altada.com>
2025-10-12 16:10:31 +0100

    hardcoding in env inside frontend

--- edge_monitor_dashboard/src/environments/environment.prod.ts
+++ edge_monitor_dashboard/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'PLACEHOLDER_BASE_URL', // gets overwritten during build
   fpApiUrl: 'PLACEHOLDER_FP_BASE_URL' // gets overwritten during build
+  // fpApiUrl: 'http://16.171.166.158:8000' // gets overwritten during build
 };
--- edge_monitor_dashboard/src/environments/environment.ts
+++ edge_monitor_dashboard/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8082',
-  fpApiUrl: 'http://16.171.166.158:8000' // gets overwritten during build
+  fpApiUrl: 'http://localhost:8000' // gets overwritten during build
+  // fpApiUrl: 'http://16.171.166.158:8000' // gets overwritten during build
 };
