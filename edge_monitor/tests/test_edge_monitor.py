026ec7215f52f8f217976ab729de21f722d96727
Bejoy Kachappilly <bejoy@altada.com>
2025-10-20 11:43:24 +0100

    Changes to edge_monitor

--- edge_monitor/tests/test_edge_monitor.py
+++ edge_monitor/tests/test_edge_monitor.py
@@ -1,22 +1,44 @@
-#!/usr/bin/env python
 import pytest
+import asyncio
+from unittest.mock import MagicMock, AsyncMock, patch
+from src.edge_monitor.edge_monitor import EdgeMonitor
+"""Tests for `edge_monitor.edge_monitor` class."""
 
-"""Tests for `edge_monitor` package."""
-
-# from edge_monitor import edge_monitor
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def test_edge_monitor_init():
     """
-    # import requests
-    # return requests.get('https://github.com/audreyfeldroy/cookiecutter-pypackage')
+        Test that EdgeMonitor initialization while passing explicit argument.
+    """
+    monitor = EdgeMonitor(
+        transport="http",
+        interval=5,
+        endpoint="http://example.com",
+        broker="localhost",
+        port=1883
+    )
+
+    assert monitor.transport == "http"
+    assert monitor.interval == 5
+    assert monitor.endpoint == "http://example.com"
+    assert monitor.broker == "localhost"
+    assert monitor.port == 1883
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.mark.asyncio
+async def test_edge_monitor_file_transfer(monkeypatch):
+    """
+        Test that EdgeMonitor calls file_transfer.send() for 'file' transport.
+    """
+    mock_file_transfer = MagicMock()
+    monkeypatch.setattr("src.edge_monitor.edge_monitor.file_transfer.send",mock_file_transfer)
+
+    my_edge_monitor = EdgeMonitor(transport="file", interval=0.1)
+    shutdown_event = asyncio.Event()
+
+    async def shut_after_delay():
+        await asyncio.sleep(0.3)
+        shutdown_event.set()
+    
+    await asyncio.gather(my_edge_monitor.run(shutdown_event), shut_after_delay())
+
+    assert mock_file_transfer.called
+    assert mock_file_transfer.call_count >=1
